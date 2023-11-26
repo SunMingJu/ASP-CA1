656edc8eec2acd1132cc1bb424bab6f75395d024
MingJu Sun <20104599@mail.wit.ie>
2023-11-26 21:51:17 +0000

    test authentication

--- movies/cypress/support/e2e.js
+++ movies/cypress/support/e2e.js
@@ -5,5 +5,15 @@ export const filterByGenre = (movieList, genreId) =>
   movieList.filter((m) => m.genre_ids.includes(genreId));
 
 export const filterByLanguage = (movieList, language) =>
-
-    movieList.filter((m) => m.original_language === language)
+  movieList.filter((m) => m.original_language === language)
+    
+export function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+  }  
+    
